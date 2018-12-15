9632aa7197159c7b9539193e3478ddd114055b50
liuwisdom <liuwisdom1@163.com>
2018-12-15 16:45:03 +0800

    sxcxsc

--- src/testwebpage/hello2.java
+++ src/testwebpage/hello2.java
@@ -2,6 +2,7 @@ package testwebpage;
 
 public class hello2 {
 	public static void main(String[] args) {
-		System.out.println("from server 2);
+		System.out.println("from server 2");
+		System.out.println("from server 3");
 	}
 }
